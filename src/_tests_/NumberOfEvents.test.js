45dcd5bda10c536bad63985a21a1a413db2a333d
Nina Wiedrich <94755570+ninawiedrich@users.noreply.github.com>
2023-09-07 12:52:04 +0200

    Implement integration tests and functionality to allow users to specify the number of events displayed in the app

--- src/_tests_/NumberOfEvents.test.js
+++ src/_tests_/NumberOfEvents.test.js
@@ -1,6 +1,7 @@
-import { render } from '@testing-library/react';
+import { render, within, waitFor } from '@testing-library/react';
 import NumberOfEvents from '../components/NumberOfEvents';
 import userEvent from '@testing-library/user-event';
+import App from '../App';
 
 describe('<NumberOfEvents /> component', () => {
   let NumberOfEventsComponent;
@@ -16,13 +17,33 @@ describe('<NumberOfEvents /> component', () => {
 
   test('default value of the input field is 32', () => {
     const textbox = NumberOfEventsComponent.getByLabelText("Number of Events:");
-    expect(textbox).toHaveValue(32);
+    expect(textbox).toHaveValue('32');
   });
 
   test('value of the textbox changes when a user types in it', async () => {
     const user = userEvent.setup();
     const textbox = NumberOfEventsComponent.getByLabelText("Number of Events:");
     await user.type(textbox, '{backspace}{backspace}10');
-    expect(textbox).toHaveValue(10);
+    expect(textbox).toHaveValue('10');
+  });
+});
+
+describe('<NumberOfEvents /> integration', () => {
+  test('number of events displayed matches the number inputted by the user', async () => {
+    const user = userEvent.setup();
+    const AppComponent = render(<App />);
+    const AppDOM = AppComponent.container.firstChild;
+
+    const NumberOfEventsDOM = AppDOM.querySelector('#numberOfEvents');
+    const numberOfEventsInput = within(NumberOfEventsDOM).queryByLabelText('Number of Events:');
+
+    await user.type(numberOfEventsInput, '{backspace}{backspace}10');
+
+    const EventListDOM = AppDOM.querySelector('#event-list');
+    const eventListItems = within(EventListDOM).queryAllByRole('listitem');
+
+    await waitFor(() => {
+      expect(eventListItems.length).toEqual(10);
+    });
   });
 });
--- src/components/NumberOfEvents.js
+++ src/components/NumberOfEvents.js
@@ -1,20 +1,30 @@
 import React, { useState } from 'react';
 
-function NumberOfEvents() {
-  const [numberOfEvents, setNumberOfEvents] = useState(32); // Default value set to 32
+const NumberOfEvents = ({ setCurrentNOE }) => {
+  const [numberOfEvents, setNumberOfEvents] = useState(32); // Set a default value of 32
+
+  const handleInputChanged = (event) => {
+    const value = event.target.value;
+    setNumberOfEvents(value);
+
+    if (setCurrentNOE) {
+      setCurrentNOE(value);
+    }
+  };
 
   return (
     <div id="numberOfEvents">
       <label htmlFor="eventNumberInput">Number of Events:</label>
       <input 
-        type="number" 
         id="eventNumberInput"
+        type="text"
         value={numberOfEvents}
-        onChange={(e) => setNumberOfEvents(e.target.value)}
-        aria-label="Number of Events"
+        onChange={handleInputChanged}
+        data-testid="numberOfEventsInput"
       />
     </div>
   );
-}
+};
 
 export default NumberOfEvents;
+
