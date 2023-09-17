4173a4ed7cb7b483f66071482dedda8c86874b6e
Nina Wiedrich <94755570+ninawiedrich@users.noreply.github.com>
2023-09-17 20:50:34 +0200

    styling of city label

--- src/components/CityEventsChart.js
+++ src/components/CityEventsChart.js
@@ -36,8 +36,8 @@ return (
       }}
     >
       <CartesianGrid />
-      <XAxis type="category" dataKey="city" name="City" />
-      <YAxis type="number" dataKey="count" name="Number of events" allowDecimals={false} />
+      <XAxis type="category" dataKey="city" name="City" angle={60} interval={0} tick={{ dx: 20, dy: 40, fontSize: 14 }} />
+      <YAxis type="number" dataKey="countnumber" name="Number of events" allowDecimals={false} />
       <Tooltip cursor={{ strokeDasharray: '3 3' }} />
       <Scatter name="A school" data={data} fill="#8884d8" />
     </ScatterChart>
