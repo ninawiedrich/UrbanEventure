5a6ab42c5508b9c7f668d27f8c7703dd80ae8ded
Nina Wiedrich <94755570+ninawiedrich@users.noreply.github.com>
2023-09-17 21:59:58 +0200

    fixed issue with chart styling

--- src/components/CityEventsChart.js
+++ src/components/CityEventsChart.js
@@ -32,12 +32,12 @@ return (
         top: 20,
         right: 20,
         bottom: 60,
-        left: 10,
+        left: -30,
       }}
     >
       <CartesianGrid />
       <XAxis type="category" dataKey="city" name="City" angle={60} interval={0} tick={{ dx: 20, dy: 40, fontSize: 14 }} />
-      <YAxis type="number" dataKey="countnumber" name="Number of events" allowDecimals={false} />
+      <YAxis type="number" dataKey="count" name="Number of events" allowDecimals={false} />
       <Tooltip cursor={{ strokeDasharray: '3 3' }} />
       <Scatter name="A school" data={data} fill="#8884d8" />
     </ScatterChart>
